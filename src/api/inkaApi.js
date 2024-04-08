6cdd37e25cc8f85dd66f5dbd418cc2da54d8890e
jhoneber31 <jhoneber31@gmail.com>
2024-04-08 15:44:04 -0500

    Products

--- src/api/inkaApi.js
+++ src/api/inkaApi.js
@@ -4,11 +4,3 @@ import axios from "axios";
 export const inkaApi = axios.create({
   baseURL: "https://inkaback-production.up.railway.app"
 })
-
-// inkaApi.interceptors.request.use( config => {
-//   config.headers = {
-//     ...config.headers,
-//     'Bearer' :  localStorage.getItem('token')
-//   }
-//   return config;
-// })
