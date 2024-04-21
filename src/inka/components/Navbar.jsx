872b3d0478ea80eae95ce4a14fdd77a0f04b06b0
Juan José Pérez Lazarte <n00220488@upn.pe>
2024-04-20 20:18:27 -0500

    Navbar.jsx: Finalización de los estilos del navbar

--- src/inka/components/Navbar.jsx
+++ src/inka/components/Navbar.jsx
@@ -1,119 +1,147 @@
-import inkaLogo from '../../shared/assets/inkaLogo.svg';
-import inkaLogoMobile from '../../shared/assets/inkaLogoMobile.svg';
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useLocationInicio } from '../../hooks/useLocationInicio';
-import { useAuthStore } from '../../hooks/useAuthStore';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import inkaLogo from "../../shared/assets/inkaLogo.svg";
+import inkaLogoMobile from "../../shared/assets/inkaLogoMobile.svg";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useLocationInicio } from "../../hooks/useLocationInicio";
+import { useAuthStore } from "../../hooks/useAuthStore";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export const Navbar = () => {
   const { showLogo, isScrolled, isAuth } = useLocationInicio();
 
   const { logoutUser, status } = useAuthStore();
 
-  const { cartTotalQuantity } = useSelector(state => state.shoppingCart);
+  const { cartTotalQuantity } = useSelector((state) => state.shoppingCart);
 
   const navigate = useNavigate();
 
   const [showProfile, setShowProfile] = useState(false);
-  const [valueMenuMobile, setValueMenuMobile] = useState(false)
+  const [valueMenuMobile, setValueMenuMobile] = useState(false);
 
   const logOut = () => {
     logoutUser();
-    navigate("/auth/login")
-  }
+    navigate("/auth/login");
+  };
 
   const showAccout = () => {
     return (
-      <div className=' bg-white absolute top-[72px] right-[0%] h-[auto] p-[15px]'>
-        {
-          (status === "authenticated")
-            ? <small
-              onClick={logOut}
-            >
-              Salir
-            </small>
-            : <NavLink
-              to={"/auth/login"}
-            >
-              Logueate
-            </NavLink>
-        }
+      <div className=" bg-white absolute top-[72px] right-[0%] h-[auto] p-[15px]">
+        {status === "authenticated" ? (
+          <small onClick={logOut}>Salir</small>
+        ) : (
+          <NavLink to={"/auth/login"}>Logueate</NavLink>
+        )}
       </div>
-    )
-  }
+    );
+  };
 
   useEffect(() => {
     console.log(status);
-  }, [])
-
+  }, []);
 
   const valueShowProfile = () => {
-    setShowProfile(!showProfile)
-  }
-
+    setShowProfile(!showProfile);
+  };
 
   return (
-    <header className={`fixed top-0 left-0 right-0 z-40 ${(isScrolled || valueMenuMobile) ? "bg-black bg-opacity-75" : "bg-transparent"}`}>
-      <div className='max-h-[67px] py-3.5 px-[15px] flex items-center gap-x-6'>
-        <div className='flex items-end gap-x-2 w-[20%]'>
-          <a className='lg:hidden' onClick={() => setValueMenuMobile(!valueMenuMobile)}>
-            <i className="fa-solid fa-bars text-white text-[30px]"></i>
-          </a>
-          <img src={inkaLogoMobile} alt="InkaLogoMobile" />
-        </div>
-        <div className={`${valueMenuMobile ? "flex" : "hidden"} flex-col items-center fixed h-[100vh] w-[100%] bg-black bg-opacity-75 py-[30px] px-[30px] top-[67px] right-0 z-50 lg:flex lg:static lg:h-[auto] lg:p-0 lg:flex-row lg:w-[80%] lg:justify-evenly lg:bg-inherit 2xl:justify-center 2xl:gap-x-40`}>
+    <header
+      className={`fixed top-0 left-0 right-0 z-40 ${
+        isScrolled || valueMenuMobile
+          ? "bg-black bg-opacity-75"
+          : "bg-transparent"
+      }`}
+    >
+      <div
+        className={`my-1 mx-3 max-h-[67px] flex items-center py-3.5  px-1.5  relative sm:my-2 sm:px-2.5 lg:justify-evenly`}
+      >
+        <a
+          className="cursor-pointer lg:hidden"
+          onClick={() => setValueMenuMobile(!valueMenuMobile)}
+        >
+          <i className="fa-solid fa-bars  text-white text-[24px]"></i>
+        </a>
+
+        <img
+          src={inkaLogoMobile}
+          alt="InkaLogoMobile"
+          className="w-[50px] mx-3 sm:mx-6 sm:w-[55px] lg:mx-0"
+        />
+
+        <div
+          className={`${
+            valueMenuMobile ? "flex" : "hidden"
+          } flex-col items-center fixed h-[100vh] w-[100%] bg-black bg-opacity-75 py-[30px] px-[30px] top-[67px] right-0 z-50  lg:flex lg:static lg:h-[auto] lg:p-0 lg:flex-row lg:w-[57%] lg:justify-evenly lg:bg-inherit 2xl:justify-center 2xl:gap-x-32`}
+        >
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? "font-bold text-tertiary text-[14px] text-center pb-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto " : "text-white font-bold text-[14px] text-center pb-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto")}
+            className={({ isActive }) =>
+              isActive
+                ? "font-bold text-tertiary text-[14px] text-center pb-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto "
+                : "text-white font-bold text-[14px] text-center pb-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto"
+            }
             onClick={() => setValueMenuMobile(false)}
           >
             INICIO
           </NavLink>
           <NavLink
             to="/productos"
-            className={({ isActive }) => (isActive ? "font-bold text-tertiary text-[14px] text-center py-[15px] border-b-2 w-full lg:p-0 lg:border-0 lg:w-auto" : "text-white font-bold text-[14px] text-center py-[15px] border-b-2 w-full lg:p-0 lg:border-0 lg:w-auto")}
+            className={({ isActive }) =>
+              isActive
+                ? "font-bold text-tertiary text-[14px] text-center py-[15px] border-b-2 w-full lg:p-0 lg:border-0 lg:w-auto"
+                : "text-white font-bold text-[14px] text-center py-[15px] border-b-2 w-full lg:p-0 lg:border-0 lg:w-auto"
+            }
             onClick={() => setValueMenuMobile(false)}
           >
             NUESTROS PRODUCTOS
           </NavLink>
           <NavLink
             to="/contactanos"
-            className={({ isActive }) => (isActive ? "font-bold text-tertiary text-[14px] text-center py-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto" : "text-white font-bold text-[14px] text-center py-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto")}
+            className={({ isActive }) =>
+              isActive
+                ? "font-bold text-tertiary text-[14px] text-center py-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto"
+                : "text-white font-bold text-[14px] text-center py-[15px] w-full border-b-2 lg:p-0 lg:border-0 lg:w-auto"
+            }
             onClick={() => setValueMenuMobile(false)}
           >
             CONTÁCTANOS
           </NavLink>
-          <div className='profile-box py-[15px] w-full text-center'>
-            <i className="fa-solid fa-user text-[25px] text-white"></i>
-          </div>
           {showProfile && showAccout()}
-
         </div>
-        <div className='flex items-end justify-end gap-x-2 w-[80%]'>
-          <div className='search-box relative'>
-            <input 
-              className='search-input rounded-[25px] pl-[35px] py-[5px] pr-[15px] w-[auto] max-w-[200px]'
-              type="text" 
-              placeholder='Buscar producto'/>
-            <i className="fa-solid fa-magnifying-glass text-gray text-[20px] absolute left-2 top-2"></i>
-          </div>
-          <div className='cart-box flex items-center'>
-            <i className="fa-solid fa-cart-shopping text-[28px] text-white"></i>
-
-            {/* <NavLink
+        <div className="search-box flex-1 relative">
+          <input
+            className="search-input h-[31px] w-[100%] rounded-[30px] text-[13px] px-2 sm:h-[35px] sm:w-[100%] sm:text-[15px] sm:px-4"
+            type="text"
+            placeholder="Buscar producto"
+          />
+          <button className="search-btn bg-transparent h-[31px] w-[31px] rounded-[30px] cursor-pointer absolute top-0 right-0 scale-90 sm:h-[35px] sm:w-[35px]">
+            <i className="fa-solid fa-magnifying-glass text-black text-[22px] sm:text-[24px]"></i>
+          </button>
+        </div>
+        <div className="flex items-center sm:mx-4">
+          <div className="mx-[14px] sm:mx-[14px] lg:mx-[18px]">
+            <NavLink
               to={"/shopping"}
-              className='text-white relative'
+              className="none text-white py-[15px] w-full flex justify-center lg:p-0 lg:border-0 lg:w-auto relative"
               onClick={() => setValueMenuMobile(false)}
             >
-              <div className='h-[25px] w-[25px] text-[14px] font-bold rounded-full bg-tertiary absolute flex items-center justify-center top-[-10px] right-[-27px]'>
+              <div className="h-[18px] w-[18px] text-[13px] font-bold rounded-full bg-tertiary absolute flex items-center justify-center top-0 right-[-18px] sm:h-[22px] sm:w-[22px] sm:text-[15px] sm:top-[-5px] sm:right-[-24px] lg:top-[-17px] lg:right-[-24px]">
                 {cartTotalQuantity}
               </div>
-              <i className="fa-solid fa-cart-shopping text-[25px]"></i>
-            </NavLink> */}
+              <i className="fa-solid fa-cart-shopping text-[22px] sm:text-[25px]"></i>
+              {/* <img src={shopIcon} alt="shopIcon" /> */}
+            </NavLink>
+          </div>
+          <div className="ms-[11px] sm:ms-[24px]">
+            <a
+              alt="profileIcon"
+              className=" text-white text-center cursor-pointer "
+              onClick={valueShowProfile}
+            >
+              <i className="fa-solid fa-user text-[22px] sm:text-[25px]"></i>
+            </a>
           </div>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
