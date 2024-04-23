07ff7a681d422aa893ca5598e1765007c17e5206
Juan José Pérez Lazarte <n00220488@upn.pe>
2024-04-22 20:06:08 -0500

    Creación de las vistas de RestorePasswordPage.jsx y ConfirmPsswordPage.jsx, Modificación en el archivo AuthRoutes.jsx

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,6 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage, RegisterPage } from "../pages"
+import { LoginPage, RegisterPage} from "../pages"
 import { Navbar } from "../../inka/components/Navbar"
+import { RestorePasswordPage } from "../pages/RestorePasswordPage"
+import { ConfirmPasswordPage } from "../pages/ConfirmPasswordPage"
 
 export const AuthRoutes = () => {
   return (
@@ -8,6 +10,8 @@ export const AuthRoutes = () => {
       <Routes>
         <Route path="login" element={ <LoginPage/> }/>
         <Route path="register" element={ <RegisterPage/> }/>
+        <Route path="restore-password" element={ <RestorePasswordPage/> }/>
+        <Route path="confirm-password" element={ <ConfirmPasswordPage/> }/>
         <Route path="/*" element={<Navigate to="/auth/login"/>}/>
       </Routes>
     </>
